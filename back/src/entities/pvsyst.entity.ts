5ac6766616f51880f01e9ad92df9152c49955c9b
benjadelcampo15 <benjadelcampo15@gmail.com>
2024-07-11 11:34:59 -0400

    Entidades creadas , (pablo gay)

---
+++ back/src/entities/pvsyst.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Panel } from './panel.entity';
+
+@Entity({ name: 'pvsyst' })
+export class Pvsyst {
+  @PrimaryGeneratedColumn('uuid')
+  id: string = uuid();
+
+  @Column()
+  month : number;
+
+  @Column()
+  estimatedGeneration : number
+
+  @ManyToOne(() => Panel, (panel) => panel.pvsyst)
+  panel: Panel;
+}
