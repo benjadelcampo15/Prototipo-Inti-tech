3ab5f5d7bfa3e4ecc2b5f1acad2341b42bf64ec0
MaxiMosquera <96501016+MaxiMosquera@users.noreply.github.com>
2024-07-15 11:37:59 -0300

    cree funcion generica para precarga de stats, y1 funcion para cada planta que guarde sus stats

--- back/src/repositories/statsPreload.repository.ts
+++ back/src/repositories/statsPreload.repository.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { StatsDto } from 'src/dtos/stats.dto';
 import { Panel } from 'src/entities/panel.entity';
 import { Stats } from 'src/entities/stats.entity';
 import { bodegasSalcobrand } from 'src/utils/bodegasSalcobrand/bodegasSalcobrand';
 import { Repository } from 'typeorm';
+import { centrovet255 } from 'src/utils/centrovet255/centrovet';
 
 @Injectable()
 export class statsPreloadRepository {
@@ -30,19 +32,55 @@ export class statsPreloadRepository {
     }
   }
 
-  async stats() {
-    const panel = await this.panelRepository.findOne({
-      where: { name: 'BODEGAS SALCOBRAND' },
+/*async stats() {
+  const panel = await this.panelRepository.findOne({
+    where: { name: 'BODEGAS SALCOBRAND' },
+  });
+  for (const stats of bodegasSalcobrand) {
+    const newStats = this.statsRepository.create({
+      day: stats.day,
+      month: stats.month,
+      year: stats.year,
+      energyGenerated: stats.energyGenerated,
+      panel: panel,
     });
-    for (const stats of bodegasSalcobrand) {
-      const newStats = this.statsRepository.create({
-        day: stats.day,
-        month: stats.month,
-        year: stats.year,
-        energyGenerated: stats.energyGenerated,
-        panel: panel,
-      });
-      await this.statsRepository.save(newStats);
-    }
+    await this.statsRepository.save(newStats);
   }
+}*/
+
+async saveStats(plantName: string, statsData: StatsDto[]) {
+  const panel = await this.panelRepository.findOne({
+    where: { name: plantName },
+  });
+
+  if (!panel) {
+    throw new Error(`Panel with name ${plantName} not found`);
+  }
+
+  for (const stats of statsData) {
+    const newStats = this.statsRepository.create({
+      day: stats.day,
+      month: stats.month,
+      year: stats.year,
+      energyGenerated: stats.energyGenerated,
+      panel: panel,
+    });
+    await this.statsRepository.save(newStats);
+  }
+}
+
+
+async saveStatsForBodegasSalcobrand() {
+  
+   
+  await this.saveStats('BODEGAS SALCOBRAND', bodegasSalcobrand);
+}
+
+// Ejemplo de uso de la función genérica para otra planta
+async saveStatsForCentrovet() {
+ 
+  await this.saveStats('Centrovet255', centrovet255);
+}
+
+
 }
