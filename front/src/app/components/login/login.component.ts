6b820c09ca62e215d1e5c3bef5997f5e406df84b
MaxiMosquera <96501016+MaxiMosquera@users.noreply.github.com>
2024-07-25 12:13:35 -0300

    authguard con redirect a login

--- front/src/app/components/login/login.component.ts
+++ front/src/app/components/login/login.component.ts
@@ -33,6 +33,7 @@ export class LoginComponent {
       });
 
       const data = await response.json();
+      localStorage.setItem("token",data.token);
       console.log(data);
 
       if (data.error) {
---
+++ front/src/app/guards/authguard.guard.ts
@@ -0,0 +1,17 @@
+
+
+
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const AuthGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const token = localStorage.getItem('token');
+  if (!token) {
+    alert("El usuario tiene que estar logueado");
+    router.navigate(['inicio/login']);
+    return false; // Previene la navegación si no hay token
+  }
+  return true; // Permite la navegación si hay token
+};
+
