d77e1a0c6adf64151d79c77a9efdceb91af9dd8d
Bruno Distaulo <0800bruno03@gmail.com>
2024-08-02 20:12:30 -0300

    deaaa

--- back/src/main.ts
+++ back/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: ['http://localhost:4200', 'prototipo-tau-two.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
