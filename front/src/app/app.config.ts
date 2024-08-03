7282ee8774a79665ded2099218554ecfc4611ade
PabloRodriguez16 <pablorodriguez6002@gmail.com>
2024-08-02 21:30:31 -0300

    je

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -1,13 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(),
-  ],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
 };
